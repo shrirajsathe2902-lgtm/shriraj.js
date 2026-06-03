7dc94745dd1de21de2248dfedaebbb60dc8ae336
shrirajsathe2902-lgtm <shrirajsathe2902@gmail.com>
2026-06-03 17:14:13 +0530

    Add files via upload

---
+++ shriraj.js66.js
@@ -0,0 +1,11 @@
+// 1. VARIABLE
+const name = "Shriraj";
+// 2. FUNCTION
+function greet(person) {
+return "Hello, " + person + "! Welcome to Nexora.";
+}
+// 3. LOOP
+const students = ["shriraj", "aditya", "yash"];
+for (let i = 0; i < students.length; i++) {
+console.log( greet(students[i]) );
+}
